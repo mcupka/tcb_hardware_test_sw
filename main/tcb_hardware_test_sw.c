66de326a2abddde0758ebb454385a57c47bf7a08
Michael Cupka <michaelcupka@hotmail.com>
2023-05-31 17:35:34 -0400

    Added .clangd for LSP     - Added sdkconfig     - LED blinking

--- main/tcb_hardware_test_sw.c
+++ main/tcb_hardware_test_sw.c
@@ -1,6 +1,20 @@
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/projdefs.h"
+#include "freertos/task.h"
+#include "hal/gpio_types.h"
 #include <stdio.h>
 
-void app_main(void)
-{
-
+void app_main(void) {
+  printf("test");
+  printf("test");
+  gpio_reset_pin(GPIO_NUM_2);
+  gpio_set_direction(GPIO_NUM_2, GPIO_MODE_OUTPUT);
+  while (1) {
+    gpio_set_level(GPIO_NUM_2, 1);
+    vTaskDelay(pdMS_TO_TICKS(1000));
+    gpio_set_level(GPIO_NUM_2, 0);
+    vTaskDelay(pdMS_TO_TICKS(1000));
+    printf("TEST\n");
+  }
 }
